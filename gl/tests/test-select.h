4b48ea2ab665c9571b7d681e2c023a5a1cd67b9d
SilvanScherrer <silvan.scherrer@aroa.ch>
2020-11-27 12:01:06 +0100

    remove a workaround, as now fixed in libcx and libc

--- gl/tests/test-select.h
+++ gl/tests/test-select.h
@@ -455,9 +455,7 @@ test_function (select_fn my_select)
   test (test_tty, "TTY", my_select);
 #endif
 
-#ifndef __OS2__ //this test should be fixed in a next libcx version
   result += test (test_bad_nfd, my_select, "Invalid nfd test");
-#endif
   result += test (test_bad_fd, my_select, "Invalid fd test");
   result += test (test_connect_first, my_select, "Unconnected socket test");
   result += test (test_socket_pair, my_select, "Connected sockets test");
